c5f536984735f6dc534fb048f6a0a4772acfd67f
Dipesh Pandey <dipeshpandey@Dipeshs-MacBook-Pro-2.local>
2023-03-26 11:56:02 +0200

    add applog model

--- applog/models.py
+++ applog/models.py
@@ -6,11 +6,12 @@ from homes.models import Home
 # create applog models here
 class AppLog(models.Model):
     app = models.CharField(max_length=100)
-    home = models.ForeignKey(Home, related_name="applog", on_delete=models.CASCADE)
-    user = models.ForeignKey(BondableUser, related_name="applog", on_delete=models.CASCADE)
+    home = models.ForeignKey(Home, on_delete=models.CASCADE)
+    in_charge = models.ForeignKey(BondableUser, on_delete=models.CASCADE, null=True, blank=True)
+    appuser = models.ForeignKey(BondableUser, related_name="userapplogs", on_delete=models.CASCADE, null=True, blank=True)
     created = models.DateTimeField()
     description = models.CharField(max_length=100)
-    data = models.JSONField(blank=True, null=True)
+    data = models.JSONField()
 
     def __str__(self):
         return f"{self.home.name} - {self.user.name}"
--- applog/serializers.py
+++ applog/serializers.py
@@ -1,62 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from .models import (
-    BondableUser,
-    Home,
-    DayShift,
-    WeeklyShiftAggregate,
-    MonthlyShiftAggregate,
-)
+from .models import AppLog
 
 
-class HomeSerializer(serializers.ModelSerializer):
-    # creator = serializers.ReadOnlyField(source="creator.username")
+class AppLogSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Home
-        fields = ("name", "address", "budget",)
-
-class DayShiftSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DayShift
-        fields = (
-            "location",
-            "day",
-            "occupancy",
-            "active_beds",
-            "admissions",
-            "discharges",
-        )
-
-class WeeklyShiftAggregateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WeeklyShiftAggregate
-        fields = (
-            "home",
-            "week_number",
-            "year",
-            "planned_admissions",
-            "planned_discharges",
-            "expected_end_of_life",
-            "agency_nurse_hours",
-            "agency_senior_carer_hours",
-            "agency_carer_hours",
-        )
-
-class MonthlyShiftAggregateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MonthlyShiftAggregate
-        fields = (
-            "home",
-            "month",
-            "year",
-            "planned_admissions",
-            "planned_discharges",
-            "expected_end_of_life",
-            "agency_nurse_hours",
-            "agency_senior_carer_hours",
-            "agency_carer_hours",
-        )
+        model = AppLog
+        fields = "__all__"
 
 
