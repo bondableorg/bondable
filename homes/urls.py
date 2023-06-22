82ee678eb7dd4840020e3a34307525d24d42487b
Dipesh Pandey <pandey.dipesh50@gmail.com>
2023-06-22 22:11:45 +0200

    fix typo

--- homes/urls.py
+++ homes/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.ListCreateLocationAPIView.as_view(), name='get_post_locations'),
     path('<int:pk>/', views.RetrieveUpdateDestroyLocationAPIView.as_view(), name='get_delete_update_location'),
     path('aggregates/<int:id>', views.get_aggregations, name='get_aggregations'),
-    path('')
 ]
